b3a9bd1d7e7d5b8e6443f4d7f7f3ec40791a9bf4
wos-beep <wos1463.1@gmail.com>
2026-03-03 16:17:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /**
  * Event Task Merger - Logic Script
- * @version 5.9.3
+ * @version 5.9.4
  * @updated 2026-03-03
  * @description 不可視文字(\u2800)による強制改行制御、8日以上半角|、大判定修正済み
  */
-const APP_VERSION = "5.9.3";
+const APP_VERSION = "5.9.4";
 
 let rawData = [];
 const fullDigits = ["０","１","２","３","４","５","６","７","８","９"];
@@ -73,29 +73,38 @@ function updateOutput() {
         combined = b.data;
     }
 
+    // --- レイアウト構成ロジック (v5.9.4: 手動成功パターンの反映) ---
     const isSlim = totalMax >= 8;
     const sep = isSlim ? "|" : "｜"; 
     const headGap = " "; 
-    // 不可視文字 \u2800 (Braille Blank) を5個並べ、確実に「文字」として認識させます
-    const rowSuffix = "\u2800\u2800\u2800\u2800\u2800"; 
+    
+    // 手動で成功した「不可視文字(点字用空白)11個」を正確に再現
+    const rowSuffix = "\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800"; 
 
     let res = title + "\n";
     if(b.id === "a") res += "商:毎日◎(SSR出せば100k~)" + rowSuffix + "\n";
     
+    // 日数ヘッダー
     res += "日数" + headGap + sep;
-    let h = [];
-    for(let i = rStart; i <= totalMax; i++) h.push(i >= 10 ? i : fullDigits[i]);
-    res += h.join(sep) + rowSuffix + "\n";
-
+    let headerNums = [];
+    for(let i = rStart; i <= totalMax; i++) {
+        headerNums.push(i >= 10 ? i : fullDigits[i]);
+    }
+    res += headerNums.join(sep) + rowSuffix + "\n";
+    
+    // データ行
     Object.keys(combined).forEach(k => {
         if (k === "行商") return;
-        let dStr = combined[k].substring(rStart - 1, totalMax);
-        if (dStr.replace(/－/g, '').length) {
-            res += k + headGap + sep + dStr.split('').join(sep) + rowSuffix + "\n";
+        let dataStr = combined[k].substring(rStart - 1, totalMax);
+        if (dataStr.replace(/－/g, '').length) {
+            // 項目名 + 半角空き + | + データ(sep結合) + 不可視の壁11個
+            const formattedData = dataStr.split('').join(sep);
+            res += k + headGap + sep + formattedData + rowSuffix + "\n";
         }
     });
 
     if(b.id === "a") res += "※7日は6日の続き(半日)" + rowSuffix;
+    
     document.getElementById('outputText').innerText = res.trim();
 }
 
